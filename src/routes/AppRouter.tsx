b62d73b52ad27beb1443b42ac29b3da9da191ad8
syaidart123 <syaidaja23@gmail.com>
2026-02-17 17:08:36 +0700

    add pages for acne scars

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,11 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
-import PageFatFreezingMalaysia from "../pages/PageFatFreezingMalaysia";
-import PageBodyContouring from "../pages/PageBodyContouring";
-import PageFraqtional from "../pages/PageFractionalLaser";
-import PageLEDPhototherapy from "../pages/PageLedPhototherapy";
-import PagePRPTreatment from "../pages/PagePrpPreg";
-import PageSubcision from "../pages/PageSubcision";
+const PageFatFreezingMalaysia = lazy(
+  () => import("../pages/PageFatFreezingMalaysia"),
+);
+const PageBodyContouring = lazy(() => import("../pages/PageBodyContouring"));
+const PageFraqtional = lazy(() => import("../pages/PageFractionalLaser"));
+const PageLEDPhototherapy = lazy(() => import("../pages/PageLedPhototherapy"));
+const PagePRPTreatment = lazy(() => import("../pages/PagePrpPreg"));
+const PageSubcision = lazy(() => import("../pages/PageSubcision"));
+const PagePeelChemical = lazy(() => import("../pages/PagePeelChimical"));
+const PageMesoBrightening = lazy(() => import("../pages/PageMesobrightening"));
+const PageMidlite = lazy(() => import("../pages/PageMidlite"));
+const PageMicroderMabrasion = lazy(
+  () => import("../pages/PageMicroderMabrasion"),
+);
+const PageBotoxTreatment = lazy(() => import("../pages/PageBotoxTreatment"));
+const PageDermalFiler = lazy(() => import("../pages/PageDermalFiler"));
+const PageSkinPeelLED = lazy(() => import("../pages/PageSkinPeelLED"));
+const PageNonSugarical = lazy(() => import("../pages/PageNonSugarical"));
+const PageActiveAcne = lazy(() => import("../pages/PageActiveAcne"));
 
 const HomePageNexus = lazy(() => import("../pages/HomePage"));
 const PageOzempicMalaysia = lazy(() => import("../pages/PageOzempicMalaysia"));
@@ -45,6 +58,17 @@ export default function AppRouter() {
           <Route path="/led-phototherapy" element={<PageLEDPhototherapy />} />
           <Route path="/prp-regenerative" element={<PagePRPTreatment />} />
           <Route path="/subcision" element={<PageSubcision />} />
+          <Route path="/chemical-peel" element={<PagePeelChemical />} />
+          <Route path="/mesobrightening" element={<PageMesoBrightening />} />
+          <Route path="/medlite" element={<PageMidlite />} />
+          <Route
+            path="/microdermabrasion"
+            element={<PageMicroderMabrasion />}
+          />
+          <Route path="/dermal-fillers" element={<PageDermalFiler />} />
+          <Route path="/mesotherapy" element={<PageMesoBrightening />} />
+          <Route path="/skin-peel-led" element={<PageSkinPeelLED />} />
+          <Route path="/acne-and-acne-scars" element={<PageActiveAcne />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
